24a753878ce19f8d7251fc8fe90c871c66982b48
Your Name <you@example.com>
2023-06-14 14:12:45 +0800

    Your commit message

---
+++ Desktop/projects/shaoxin/src/App.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+  data() {
+    return {
+      message: 'Hello World.'
+    }
+  },
+  methods: {
+    fanzhuanMessage() {
+      this.message = this.message.split('').reverse().join('')
+    },
+    tanchuang() {
+      alert('guanbitanchuang')
+    }
+  }
+}
+</script>
+
+<template>
+<h1>{{ message }}</h1>
+<button @click="fanzhuanMessage">fanzhuanMessage</button>
+<button @click="tanchuang">www.baidu.com</button>
+<button @click="message += '!'">dianjijia"!"</button>
+<a href="www.jingdong.com" @click.prevent="tanchuang">
+  dakaizhegewangzhan
+
+</a>
+
+</template>
+
+<style>
+
+
+</style>
